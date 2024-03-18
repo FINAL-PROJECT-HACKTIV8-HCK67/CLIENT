1b3b7cd40ff4c497de6acd3c20c9526b7cb2c0cc
ridhakurfi <ridhakurfi@gmail.com>
2024-03-18 15:31:40 +0700

    added global css

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -44,7 +44,7 @@ export default function Register() {
               />
             </div>
             <div className="mt-6">
-              <button className="w-full inline-flex items-center justify-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-blue-700 active:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition">
+              <button className="w-full inline-flex items-center justify-center px-4 py-2 bg-teal-300 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-teal-400 active:bg-teal-700 focus:outline-none focus:border-teal-700 focus:ring focus:ring-teal-200 disabled:opacity-25 transition">
                 Sign up
               </button>
             </div>
