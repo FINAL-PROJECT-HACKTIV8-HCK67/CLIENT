96ed3232ac4c4d59ac36fa4d562cc7a5a4786128
ridhakurfi <ridhakurfi@gmail.com>
2024-03-19 13:45:19 +0700

    refactor router

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -3,14 +3,10 @@ import LandingPage from "./pages/landingPage";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Courses from "./pages/course";
-<<<<<<< HEAD
-import Profile from "./pages/profile";
-=======
 import Layout from "./pages/layout";
 import CourseId from "./pages/courseId";
 import Quiz from "./pages/quiz";
 import Achievement from "./pages/achievement";
->>>>>>> f726b1a2f4e0b1f5567966dbfa5cb0c730d774cb
 export const router = createBrowserRouter([
   {
     path: "/",
